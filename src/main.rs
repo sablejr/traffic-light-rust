5bec8f7c24417b0af1b17850031f06c84715295e
Marc Dugas <sablejr@gmail.com>
2026-01-02 01:09:17 -0500

    initial commit (1 traffic light)
    Added traffic light simulation with state transitions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::thread;
+use std::time::Duration;
+
+#[derive(Debug)]
+enum LightColor {
+    Red,
+    Yellow,
+    Green,
+}
+
+//The function only needs to inspect the state to determine the next one
+fn next_state(current: &LightColor) -> LightColor {
+    match current {
+        LightColor::Red => LightColor::Green,
+        LightColor::Green => LightColor::Yellow,
+        LightColor::Yellow => LightColor::Red,
+    }
+}
+
+fn transition_time(current: &LightColor) -> Duration {
+    match current {
+        LightColor::Red => Duration::from_secs(3),
+        LightColor::Green => Duration::from_secs(3),
+        LightColor::Yellow => Duration::from_secs(1),
+    }
+}
+
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut state = LightColor::Red;
+    loop {
+        println!("Current state: {:?}", state);
+        thread::sleep(transition_time(&state));
+        state = next_state(&state);
+    }
 }
